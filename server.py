bbf21054bca8295c020dfd809e20954390ca58c9
Colin Bowers <colin.bowers@gmail.com>
2021-02-17 11:35:00 -0500

    Added /register command.  Need to register before particpating in the chat.

--- server.py
+++ server.py
@@ -10,8 +10,8 @@ class ChatServer():
         self.port = 9009
         self.buf_size = 4096
         self.max_clients = 10
-        self.socket_list = [ ]
-        self.clients = [ ]
+        self.socket_list = [ ]  # list of socket objects
+        self.registered = { }   # registered users: sockfd => name
 
 
     def start(self):
@@ -55,32 +55,55 @@ class ChatServer():
                             self.do_command(sock, data_str)
                         else:
                             # chat message
-                            name = self.get_socket_name(sock)
-                            msg = f"\r[{self.get_socket_name(sock)}] {data_str}"
-                            self.broadcast(sock, msg)
+                            client_name = self.get_client_name(sock)
+                            if client_name is not None:
+                                msg = f"\r[{client_name}] {data_str}"
+                                self.broadcast(sock, msg)
+                            else:
+                                msg = f"\r> please register first, use /register <name>\n"
+                                self.send(sock, msg)
                     else:
                         #remove the socket that's broken
                         self.remove_client(sock)
 
 
-    def get_socket_name(self, sock):
+    def is_registered(self, sockfd):
+        for name in list(self.registered):
+            if sockfd == self.registered[name]:
+                return True
+        return False
+
+
+    def get_client_name(self, sockfd):
+        for name in list(self.registered):
+            if sockfd == self.registered[name]:
+                return name
+        return None
+
+
+    def get_connection_name(self, sock):
         p = sock.getpeername()
         return f"{p[0]}:{p[1]}"
 
 
     def add_client(self, sockfd, addr):
         self.socket_list.append(sockfd)
-        self.clients.append(sockfd)
-        name = self.get_socket_name(sockfd)
-        self.broadcast(sockfd, f"\r> new connection {name}\n")
+        name = self.get_connection_name(sockfd)
+        #self.broadcast(sockfd, f"\r> new connection {name}\n")
+        self.log(f"\r> new connection from {name}\n")
 
 
     def remove_client(self, socket):
-        name = self.get_socket_name(socket)
-        self.broadcast(socket, f"\r> connection {name} dropped\n")
+        conn_name = self.get_connection_name(socket)
         if socket in self.socket_list:
             self.socket_list.remove(socket)
-            self.clients.remove(socket)
+            client_name = self.get_client_name(socket)
+            if client_name is not None:
+                del(self.registered[client_name])
+                self.broadcast(socket, f"\r> {client_name} has left the room\n")
+            else:
+                self.log(f"\r> unregistered client from {conn_name} dropped\n")
+
         socket.close()
 
 
@@ -94,7 +117,7 @@ class ChatServer():
 
     def broadcast(self, sender_socket, message):
         self.log(message)
-        for s in self.socket_list:
+        for name, s in self.registered.items():
             if s != self.server_socket and s != sender_socket:
                 try:
                     s.send(message.encode('utf-8'))
@@ -111,7 +134,7 @@ class ChatServer():
         cmd = message[1:].strip().split()
         self.log(f"/{cmd[0]} from {sockfd.getpeername()}")
 
-        if cmd[0] in ("help", "who"):
+        if cmd[0] in ("help", "who", "register"):
             try:
                 # dynamically call a function based on the command name
                 method_name = "do_" + cmd[0]
@@ -125,12 +148,23 @@ class ChatServer():
             self.send(sockfd, f"\r> sorry, '{cmd[0]}' is not implemented.\n")
 
 
+    def do_register(self, sockfd, args):
+        if len(args) < 1:
+            msg = f"\r> missing nickname.  usage: /register <nick>\n"
+            self.send(sockfd, msg)
+            return
+
+        name = args[0]
+        self.broadcast(sockfd, f"\r> {name} has entered the chat room\n")
+        self.registered[name] = (sockfd)
+
+
     def do_who(self, sockfd, args):
         #self.send(sockfd, f"\r> who\n")
-        msg = "\r> Currently connected:\n"
-        for s in self.clients:
-            msg += f">  {self.get_socket_name(s)}"
-            if s == sockfd:
+        msg = "\r> Currently registered in this chat room:\n"
+        for name in list(self.registered):
+            msg += f">  {name}"
+            if sockfd == self.registered[name]:
                 msg += " (you)"
             msg += "\n"
 
